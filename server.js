04f71faaef88c9cbe956f627f4bddfc2921b098d
Raheel Ahmed <raheel25031@gmail.com>
2026-07-21 21:47:15 +0500

    First commit

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config();
+
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+var users = []
+
+app.get("/api/health", (req, res) => {
+    res.send("Hello");
+});
+
+
+app.post("/api/create-user", (req, res) => {
+    const {email, password} = req.body
+
+    const userObj = {
+        email,
+        password
+    }
+    users.push(userObj);
+    res.status(200).json({
+        message: "user created success",
+        status: true,
+        data: userObj
+    })
+})
+
+app.get("/api/all", (req, res) => {
+
+    
+    res.status(200).json({
+        message: "User Fetched!",
+        data: users
+    })
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
